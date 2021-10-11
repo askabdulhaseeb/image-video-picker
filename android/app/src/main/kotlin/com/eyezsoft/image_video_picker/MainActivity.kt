a6f2dc021d8e18fc1e1299968d97b70ea81a3419
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-10-12 01:27:11 +0500

    init

---
+++ android/app/src/main/kotlin/com/eyezsoft/image_video_picker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eyezsoft.image_video_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/landing_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({Key? key}) : super(key: key);
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
