3d53bce64fda245012141aa7bacc2b22307cc711
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-10-12 12:07:51 +0500

    video and image added.

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_video_picker/video_widget.dart';
+import 'package:video_player/video_player.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({Key? key}) : super(key: key);
@@ -7,8 +11,80 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+  int lastTap = 0;
+  int consecutiveTaps = 0;
+  List<XFile>? _imageFileList;
+  XFile? _videoFile;
+  int index = 0;
+  bool _show = false;
+  bool isVideo = false;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image Video Picker'),
+      ),
+      body: GestureDetector(
+        onTap: () async {
+          int now = DateTime.now().millisecondsSinceEpoch;
+          if (now - lastTap < 1000) {
+            consecutiveTaps++;
+            if (consecutiveTaps == 3) {
+              try {
+                final ImagePicker _picker = ImagePicker();
+                final pickedFileList = await _picker.pickMultiImage();
+                final video =
+                    await _picker.pickVideo(source: ImageSource.gallery);
+                setState(() {
+                  _imageFileList = pickedFileList;
+                  _videoFile = video;
+                  _show = true;
+                });
+                print('Length: ${_imageFileList!.length}');
+              } catch (e) {
+                print(e.toString());
+              }
+            }
+          } else {
+            consecutiveTaps = 0;
+          }
+          lastTap = now;
+        },
+        child: Center(
+          child: Container(
+            width: double.infinity,
+            height: 200,
+            color: Colors.green,
+            alignment: Alignment.center,
+            child: (_show)
+                ? GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (index == 1) {
+                          index = 0;
+                        } else {
+                          index = 1;
+                        }
+                      });
+                    },
+                    onDoubleTap: () {
+                      setState(() {
+                        isVideo = !isVideo;
+                      });
+                    },
+                    child: (isVideo)
+                        ? VideoWidget(path: _videoFile!.path)
+                        : Image.file(
+                            File(_imageFileList![index].path),
+                            fit: BoxFit.cover,
+                          ),
+                  )
+                : const Icon(Icons.attachment_sharp),
+          ),
+        ),
+      ),
+    );
   }
 }
+
+// https://stackoverflow.com/questions/60658738/how-to-create-triple-click-button-in-flutter
---
+++ lib/video_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoWidget extends StatefulWidget {
+  final String _path;
+
+  const VideoWidget({
+    required String path,
+    Key? key,
+  })  : _path = path,
+        super(key: key);
+  @override
+  _VideoAppState createState() => _VideoAppState();
+}
+
+class _VideoAppState extends State<VideoWidget> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(widget._path)
+      ..addListener(() => setState(() {}))
+      ..setLooping(true)
+      ..initialize().then((_) => _controller.play());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return VideoPlayer(_controller);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
