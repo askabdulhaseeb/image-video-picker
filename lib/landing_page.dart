9e83fd633cf0988a74c3be960963dd7f6b2c4f0e
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-10-15 15:14:15 +0500

    fix extensions issue

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:file_picker/file_picker.dart';
+import 'package:file_picker/src/file_picker_io.dart';
 import 'package:flutter/material.dart';
 import 'package:image_video_picker/video_widget.dart';
 
@@ -26,62 +27,63 @@ class _LandingPageState extends State<LandingPage> {
       setState(() {});
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Image Video Picker'),
-      ),
-      body: GestureDetector(
-        onTap: () async {
-          int now = DateTime.now().millisecondsSinceEpoch;
-          if (now - lastTap < 1000) {
-            consecutiveTaps++;
-            if (consecutiveTaps == 2) {
-              final FilePickerResult? _result =
-                  await FilePicker.platform.pickFiles(
-                allowMultiple: true,
-                // type: FileType.any,
-                // allowedExtensions: ['.jpeg', '.jpg', '.mp4', '.mov'],
-              );
-              if (_result == null) return;
-              _files = _result.files;
-              setState(() {});
-              _files.forEach((element) {
-                print(element.extension);
-              });
-            }
-          } else {
-            consecutiveTaps = 0;
+    return GestureDetector(
+      onTap: () async {
+        int now = DateTime.now().millisecondsSinceEpoch;
+        if (now - lastTap < 1000) {
+          consecutiveTaps++;
+          if (consecutiveTaps == 2) {
+            final FilePickerResult? _result =
+                await FilePicker.platform.pickFiles(
+              allowMultiple: true,
+              type: FileType.custom,
+              allowedExtensions: ['jpeg', 'jpg', 'mp4', 'mov'],
+            );
+            if (_result == null) return;
+            _files = _result.files;
+            setState(() {});
           }
-          lastTap = now;
-        },
-        child: Center(
-          child: Container(
-            width: double.infinity,
-            height: 200,
-            color: Colors.green,
-            alignment: Alignment.center,
-            child: (_files.isNotEmpty)
-                ? (_files[index].extension == '.jpeg' ||
-                        _files[index].extension == '.jpg')
-                    ? GestureDetector(
-                        onTap: _onTap,
-                        child: Image.file(
-                          File(_files[index].path!),
-                          fit: BoxFit.cover,
-                        ),
-                      )
-                    : (_files[index].extension == '.mp4' ||
-                            _files[index].extension == '.mov')
-                        ? GestureDetector(
-                            onDoubleTap: _onTap,
-                            child: VideoWidget(path: File(_files[index].path!)),
-                          )
-                        : GestureDetector(
-                            onTap: _onTap,
-                            child: const Text('Invalid File Selected'),
-                          )
-                : const Icon(Icons.attachment_sharp),
-          ),
+        } else {
+          consecutiveTaps = 0;
+        }
+        lastTap = now;
+      },
+      child: Scaffold(
+        body: Center(
+          child: (_files.isNotEmpty)
+              ? (_files[index].extension == 'jpeg' ||
+                      _files[index].extension == 'jpg')
+                  ? GestureDetector(
+                      onTap: _onTap,
+                      child: Image.file(
+                        File(_files[index].path!),
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                  : (_files[index].extension == 'mp4' ||
+                          _files[index].extension == 'mov')
+                      ? GestureDetector(
+                          onDoubleTap: _onTap,
+                          child: VideoWidget(path: File(_files[index].path!)),
+                        )
+                      : GestureDetector(
+                          onTap: _onTap,
+                          child: const Text('Invalid File Selected'),
+                        )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Text(
+                      'Welcome',
+                      style:
+                          TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Tap anywhere Three time to select files',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
         ),
       ),
     );
